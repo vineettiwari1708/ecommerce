f58586d3793b6392a1eaf8257b4943d1849cb48b
unknown <vineettiwari1708@gmail.com>
2025-01-19 12:02:02 +0530

    start project with css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./css/style.css";
+
+function App() {
+  return (
+    <>
+      <h1>This is my React App</h1>
+      <h2>This is second line of React App</h2>
+      <img src="1.jpeg" />
+    </>
+  );
+}
+
+export default App;
